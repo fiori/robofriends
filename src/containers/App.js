fdb2b27db7e6c00b963e18ce045e1e5d882653ee
Fiori <flavio.fiori@naughtysquirrels.net>
2023-05-16 16:46:03 +0100

    added react hooks, useState and useEffect

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import SearchBox from '../components/SearchBox';
 import CardList from '../components/CardList';
@@ -6,45 +6,38 @@ import Sticky from '../components/Sticky';
 import './App.css';
 import ErrorBoundry from '../components/ErrorBoundry';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: '',
-    };
-  }
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState('');
 
-  onSearchChange = event => {
-    this.setState({ searchfield: event.target.value });
+  const onSearchChange = event => {
+    setSearchField(event.target.value);
   };
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(r => r.json())
-      .then(users => this.setState({ robots: users }));
-  }
+      .then(users => setRobots(users));
+    console.log('Fetch');
+  }, []);
 
-  render() {
-    const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter(r => {
-      return r.name.toLowerCase().includes(searchfield.toLowerCase());
-    });
+  const filteredRobots = robots.filter(r => {
+    return r.name.toLowerCase().includes(searchField.toLowerCase());
+  });
 
-    return !robots.length ? (
-      <h1>Loading</h1>
-    ) : (
-      <div className='tc'>
-        <Sticky>
-          <h1 className='tc f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-        </Sticky>
-        <ErrorBoundry>
-          <CardList robots={filteredRobots}></CardList>
-        </ErrorBoundry>
-      </div>
-    );
-  }
+  return !robots.length ? (
+    <h1>Loading</h1>
+  ) : (
+    <div className='tc'>
+      <Sticky>
+        <h1 className='tc f1'>RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange} />
+      </Sticky>
+      <ErrorBoundry>
+        <CardList robots={filteredRobots}></CardList>
+      </ErrorBoundry>
+    </div>
+  );
 }
 
 export default App;
